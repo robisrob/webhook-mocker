7cf6ff2c46b3054ebe0d3b93c4165b4a5444621b
Rob Swartenbroekx <rob.s@sparkcentral.com>
2021-08-11 06:23:55 +0200

    setup

---
+++ src/main/java/webhook/mocker/robisrob/github/com/webhookmocker/WebhookMockerApplication.java
@@ -0,0 +1,13 @@
+package webhook.mocker.robisrob.github.com.webhookmocker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebhookMockerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebhookMockerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/webhook/mocker/robisrob/github/com/webhookmocker/WebhookMockerApplicationTests.java
@@ -0,0 +1,13 @@
+package webhook.mocker.robisrob.github.com.webhookmocker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebhookMockerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
